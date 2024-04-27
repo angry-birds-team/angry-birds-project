7cdafa600a13d097108241ce413c7629393fe31e
kotaflow23 <kotaflow@outlook.com>
2024-04-27 16:30:25 -0500

    added file dialog to save workbook, fixed bugged with starting to open file dialog but not actually selecting a file

--- sprint-four/codebase/main.py
+++ sprint-four/codebase/main.py
@@ -23,7 +23,7 @@ print(path)
 # remember, normal current cwd is angry-birds-project
 codebase_path = "sprint-four/codebase/"
 models_path = "sprint-four/models/"
-input_path = "sprint-four/models/"
+input_path = "sprint-four/input/"
 output_path = "sprint-four/output/"
 
 
@@ -86,11 +86,17 @@ def open_files(): #function to a set of files
     global input_video_paths
     global cap
     global frame_number
-    global first_sheet
+    global sheet
 
     # open system dialog to open video files
-    paths_tuple = filedialog.askopenfilenames(title="Select Video Files", filetypes=(("MP4 files", "*.mp4"), ("All files", "*.*")))
+    paths_tuple = filedialog.askopenfilenames(title="Select Video Files", filetypes=(("MP4 files", "*.mp4"), ("All files", "*.*")), initialdir=f"{path}/{input_path}")
 
+    print(f"!!\nPaths Tuple: {paths_tuple}\n!!")
+
+    # if the user doesn't select a file, abort
+    if paths_tuple is None or len(paths_tuple)==0:
+        return
+    
     input_video_paths.extend(paths_tuple)
     input_video_paths.reverse()
 
@@ -99,7 +105,7 @@ def open_files(): #function to a set of files
         cap.release()
     cap = cv2.VideoCapture(input_video_paths[-1])
     video_selected = os.path.basename(input_video_paths[-1])
-    video_label.config(text=f"Video Selected:\n\n {video_selected}", font=("Terminal", 20))
+    video_label.config(text=f"Current Video Selected:\n\n {video_selected}", font=("Terminal", 20))
     success, preview_image = cap.read()
     if success:
         # Convert image from one color space to other
@@ -129,9 +135,9 @@ def open_files(): #function to a set of files
     cap = cv2.VideoCapture(first_video)
 
     # create first sheet for first video
-    first_sheet = global_workbook.active
-    first_sheet.append(["Timesheet for:", f"{os.path.basename(first_video)}"])
-    first_sheet.append(["Start Time (min:sec)", "End Time (min:sec)"])
+    sheet = global_workbook.active
+    sheet.append(["Timesheet for:", f"{os.path.basename(first_video)}"])
+    sheet.append(["Start Time (min:sec)", "End Time (min:sec)"])
 
 
 def detect_bird(frame): #function for detecting the bird in a frame
@@ -197,7 +203,7 @@ def thresholding(checked_frame):    # function for thresholding based on confide
                     seconds_end = int(timestamp_end % 60)
                     timestamp_start_string = f"{minutes_start:02}:{seconds_start:02}"
                     timestamp_end_string = f"{minutes_end:02}:{seconds_end:02}"
-                    first_sheet.append([timestamp_start_string, timestamp_end_string])
+                    sheet.append([timestamp_start_string, timestamp_end_string])
                     timestamp_start = None
                     delay_started = False
     #Testing delay
@@ -314,12 +320,12 @@ def read_capture():
         time_position_label.config(text=f"Timestamp: {time_position}")
 
         # Update the timestamp label under "Arrivals & Departures"
-        timestamps = "\n".join([f"{first_sheet.cell(row=i, column=1).value} - {first_sheet.cell(row=i, column=2).value}" for i in range(1, first_sheet.max_row+1)])
+        timestamps = "\n".join([f"{sheet.cell(row=i, column=1).value} - {sheet.cell(row=i, column=2).value}" for i in range(1, sheet.max_row+1)])
         arrivals_departures_text.delete(1.0, END)  # Clear the text widget
         arrivals_departures_text.insert(END, timestamps)
 
         # Check if end of video is reached
-        if cap.get(cv2.CAP_PROP_POS_FRAMES) < cap.get(cv2.CAP_PROP_FRAME_COUNT):
+        if cap.get(cv2.CAP_PROP_POS_FRAMES) < cap.get(cv2.CAP_PROP_FRAME_COUNT): # if not, keeping going
             if playing:
                 image_widget.after(1, read_capture)
         else: # if it has been, load next video
@@ -331,7 +337,9 @@ def read_capture():
                 save_workbook()
             else: # if another video is left, update the video label gui element and open the capture to that new video
                 video_selected = os.path.basename(input_video_paths[-1])
-                video_label.config(text=f"Video Selected:\n\n {video_selected}", font=("Terminal", 20))
+                sheet.append(["Timesheet for:", f"{os.path.basename(video_selected)}"])
+                sheet.append(["Start Time (min:sec)", "End Time (min:sec)"])
+                video_label.config(text=f"Current Video Selected:\n\n {video_selected}", font=("Terminal", 20))
                 cap.open(input_video_paths.pop())
             if playing:
                 image_widget.after(1, read_capture)
@@ -343,7 +351,10 @@ def save_workbook():
         # Extract the base name of the video file
         video_base_name = os.path.splitext(os.path.basename(input_video_path))[0]
         # Save the workbook with the video's base name as the file name
-        global_workbook.save(f"{output_path}/timestamps.xlsx")
+        #save_workbook_location = filedialog.asksaveasfilename(title="Save File As", filetypes=(("Excel File", "*.xlsx"), ("All files", "*.*")))
+        save_workbook_location = filedialog.asksaveasfilename(title="Save Workbook As...", filetypes=(("Excel File", "*.xlsx"), ("All files", "*.*")), defaultextension=".xlsx", initialdir=f"{path}/{output_path}")
+        #global_workbook.save(f"{output_path}/timestamps.xlsx")
+        global_workbook.save(save_workbook_location)
         print("Workbook saved successfully")
     except Exception as e:
         print(f"Failed to save workbook: {e}")
@@ -389,7 +400,7 @@ if __name__ == "__main__":
     model_label = ttk.Label(left_frame, text=f"Model Selected:\n\n {model_selected.capitalize()}", font=("Terminal", 20))
     model_label.pack(side=TOP, anchor=W, padx=10, pady=10)
     # Create a label to display the currently selected video file
-    video_label = ttk.Label(left_frame, text=f"Video Selected:\n\n None", font=("Terminal", 20))
+    video_label = ttk.Label(left_frame, text=f"Current Video Selected:\n\n None", font=("Terminal", 20))
     video_label.pack(side=TOP, anchor=W, padx=10, pady=10)
     # test button
     # test_button = ttk.Button(left_frame, text="TEST", command=read_capture,) # logic not implemented
